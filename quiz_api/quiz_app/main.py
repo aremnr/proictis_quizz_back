7509c2f38464b865ca82ea9d1f8935a312e687cb
aremnr <armennen941@gmail.com>
2024-10-12 15:26:02 +0300

    off cors settings in code

--- quiz_api/quiz_app/main.py
+++ quiz_api/quiz_app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from quiz_app.routers import quiz, game, user, admin_side_html
 from admin_side.main import router as admin_side_router
-from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="test",
 )
